ad11fdd35456e5e238f3fd2178344173757cf04f
Sergey Klyushin <sklyushin@elvees.com>
2019-03-26 17:58:21 +0300

    swic-loopback-test: Add test_flush_fifo
    Test is used to check flush fifo
    
    Change-Id: Icc2fb7b8c210858e7f90c806b8a31e2052807d8c

--- swic-loopback-test.py
+++ swic-loopback-test.py
@@ -2,12 +2,36 @@
 
 # Copyright 2019 RnD Center "ELVEES", JSC
 
+from fcntl import ioctl
 import filecmp
 import math
 import os
 import random
+import struct
 import subprocess
+import tempfile
 import unittest
+
+_IOC_TYPEBITS = 8
+_IOC_NRBITS = 8
+_IOC_NRSHIFT = 0
+_IOC_SIZEBITS = 14
+_IOC_WRITE = 1
+_IOC_TYPESHIFT = _IOC_NRSHIFT + _IOC_NRBITS
+_IOC_SIZESHIFT = _IOC_TYPESHIFT + _IOC_TYPEBITS
+_IOC_DIRSHIFT = _IOC_SIZESHIFT + _IOC_SIZEBITS
+
+
+def _IOC(dir, type, nr, size):
+    return int((dir << _IOC_DIRSHIFT) | (type << _IOC_TYPESHIFT) |
+               (nr << _IOC_NRSHIFT) | (size << _IOC_SIZESHIFT))
+
+
+def _IOW(type, nr, size):
+    return _IOC(_IOC_WRITE, type, nr, struct.calcsize(size))
+
+
+SWICIOC_SET_LINK = _IOW(ord('w'), 1, 'i')  # define SWICIOC_SET_LINK _IOW('w', 1, int)
 
 
 class TestcaseSWIC(unittest.TestCase):
@@ -97,6 +121,49 @@ class TestcaseSWIC(unittest.TestCase):
                     print('Iteration {}, mtu={}'.format(i+1, mtu))
                 with self.subTest(iter=i, mtu=mtu):
                     self.check(speed, mtu, '/dev/spacewire0', '/dev/spacewire1')
+
+    def read32(self, addr):
+        return int((subprocess.check_output(['devmem',
+                                             hex(addr)]).strip()).decode('UTF-8'), 16)
+
+    def wait_event(self, mask, value):
+        rx_status = self.read32(0x38084004)
+        while rx_status & mask != value:
+            rx_status = self.read32(0x38084004)
+
+    def setnbreak_link(self, device):
+        with open(device, 'w') as handle:
+            ioctl(handle, SWICIOC_SET_LINK, 1)
+
+            # Waiting for set link
+            self.wait_event(0xF0, 0xA0)
+
+            # Waiting for fill RX FIFO
+            self.wait_event(0x100, 0x100)
+
+            ioctl(handle, SWICIOC_SET_LINK, 0)
+
+    def test_flush_fifo(self):
+        rxfifo_size = 384
+        desc_size = 16 * 1024
+        num_descs = 65
+        rxring_size = desc_size * num_descs
+        filesize = rxring_size + rxfifo_size
+        mtu = filesize / 2
+        speed = 8
+
+        input_temp = tempfile.NamedTemporaryFile()
+        input_temp.write(os.urandom(filesize))
+        proc = subprocess.Popen(['swic-xfer', input_temp.name,
+                                 '/dev/spacewire0',
+                                 '-s', str(speed),
+                                 '-m', str(mtu)])
+        self.setnbreak_link('/dev/spacewire1')
+
+        proc.kill()
+        proc.wait()
+
+        self.check(speed, 1024, '/dev/spacewire0', '/dev/spacewire1')
 
 
 if __name__ == '__main__':
